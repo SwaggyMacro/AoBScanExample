21ebdc1d41950448ac444722dfffbf7c3dba8f08
SwaggyMacro <macro@julym.com>
2023-08-18 11:48:24 +0800

    Add project files.

---
+++ AoBScanExample/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace AoBScanExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(85, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(309, 87);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(306, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Scan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(30, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Content";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(30, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(85, 15);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 23);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "WeChatAppEx";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(225, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Title";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox3.Location = new System.Drawing.Point(274, 16);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(120, 23);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "羊了个羊";
+            // 
+            // listView1
+            // 
+            this.listView1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(32, 178);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(363, 238);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 436);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ AoBScanExample/Form1.cs
@@ -0,0 +1,69 @@
+﻿using Memory;
+using System;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AoBScanExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.View = View.Details;
+            listView1.Columns.Add("Id", 100);
+            listView1.Columns.Add("Data", 200);
+            listView1.LabelEdit = true;
+            listView1.ItemActivate += ListView1_ItemActivate;
+            listView1.FullRowSelect = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(AoBScan(textBox2.Text, textBox3.Text, textBox1.Text));
+        }
+        public string AoBScan(string processName, string windowTitle, string scanContent)
+        {
+            scanContent = BitConverter.ToString(Encoding.ASCII.GetBytes(scanContent)).Replace("-", " ");
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                if (process.ProcessName.Equals(processName) && process.MainWindowTitle.Equals(windowTitle))
+                {
+                    Mem m = new Mem();
+                    m.OpenProcess(process.Id);
+                    var addrList = m.AoBScan(scanContent, true, true, true).Result;
+                    foreach (var addr in addrList)
+                    {
+                        Console.WriteLine(addr);
+
+                        // read addr context
+                        var token = m.ReadString(addr.ToString("X"), length: 300).ToString();
+                        Console.WriteLine(token);
+
+                        // insert item to listview
+                        string[] item = { (listView1.Items.Count + 1).ToString(), token };
+                        listView1.Items.Add(new ListViewItem(item));
+
+                        // it should be token if length >= 135
+                        if (token.Length >= 135)
+                        {
+                            return token;
+                        }
+                    }
+                }
+            }
+            return "false";
+        }
+        private void ListView1_ItemActivate(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string column2Text = listView1.SelectedItems[0].SubItems[1].Text;
+                Clipboard.SetText(column2Text);
+                MessageBox.Show("Copy to clipboard: " + column2Text, "Tips", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
